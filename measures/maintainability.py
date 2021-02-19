1196c5c57ffe4b9605a489cebdab3be72ee5b677
gva-jjoyce <57803985+gva-jjoyce@users.noreply.github.com>
2021-02-19 12:41:04 +0000

    Create maintainability.py

---
+++ measures/maintainability.py
@@ -0,0 +1,56 @@
+"""
+Maintainability Index Tester
+
+Uses radon to calculate Maintainability Index
+(see: https://radon.readthedocs.io/en/latest/intro.html)
+
+
+Files in /tests/ folder are excluded, as well as as there being an option to
+add a flag to the file to exclude specific files.
+
+Radon itself will A grade for maintainability for scores 100 to 20, this
+script sets the bar at 65.
+"""
+import radon.metrics
+import logging
+import glob
+import sys
+
+logger = logging.getLogger("measures")
+
+EXCLUSIONS = ['/tests/']
+LIMIT = 65
+
+class MaintainabilityTest():
+
+    def __init__(self):
+        pass
+
+    def test(self):
+
+        file_list = glob.iglob('./**/*.py', recursive=True)
+        results = []
+
+        for item in file_list:
+            if any([True for exclusion in EXCLUSIONS if item.startswith(exclusion)]):
+                continue
+
+            with open(item, 'r', encoding='UTF8') as code_file:
+                code = code_file.read()
+
+            maintainability_index = radon.metrics.mi_visit(code, True)
+
+            if code.startswith('#no-maintain-checks'):
+                logger.info(F"{item:20} {maintainability_index:.2f} - skipped")
+                results.append('SKIPPED')
+                continue
+
+            if maintainability_index <= LIMIT:
+                results.append('FAILED')
+                logger.error(F"{item:20} {maintainability_index:.2f} - below {LIMIT}")
+            else:
+                results.append('PASSED')
+
+        logger.info(F"MAINTAINABILITY INDEX: {results.count('PASSED')} passed, {results.count('FAILED')} failed, {results.count('SKIPPED')} skipped")
+
+        return results.count('FAILED') > 0
